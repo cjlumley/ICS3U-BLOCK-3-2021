8ca0859c2e8477c484ed06e27f99065c2932a4d9
COOPER LUMLEY <clumley@bayviewglen.ca>
2021-09-09 13:18:31 -0400

    Week 1

---
+++ UNIT 1/src/week1/Example2.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class Example2 {
+    public static void main(String[] args) {
+        System.out.println("A"); // Prints A and hits enter
+        System.out.println("B"); // Prints B and hits enter
+        System.out.print("C"); // Prints C
+        System.out.println("D"); // Prints D and hits enter 
+        System.out.println("E"); // Prints E and hits enter
+    }
+    
+}
